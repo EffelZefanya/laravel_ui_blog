91201b44bfba996d2a77563e0a623304d1f4c54b
Fel <99955631+EffelZefanya@users.noreply.github.com>
2023-07-24 10:02:24 +0700

    feat:add eloquent relationship to models

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Article extends Model
 {
@@ -14,4 +16,14 @@ class Article extends Model
         'content',
         'image',
     ];
+
+    public function category(): HasMany
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
