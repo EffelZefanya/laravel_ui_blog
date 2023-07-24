b6539974cec125c95f32c7d8dfd55333f40f9abc
Fel <99955631+EffelZefanya@users.noreply.github.com>
2023-07-24 09:30:32 +0700

    feat:update migrate article and category

--- database/migrations/2023_07_24_021431_create_articles_table.php
+++ database/migrations/2023_07_24_021431_create_articles_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string("title");
+            $table->text("content");
+            $table->string("image");
+            $table->foreign("user_id")->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign("category_id")->references('id')->on('categories')->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
         });
     }
